0cbceae522754e125fa222f82a9587ef8095c654
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-01-20 22:19:12 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -114,143 +114,6 @@ fn main() {
                 } else {
                     //println!("already cloned, skipping")
                 }
-
-                // TODO FIXME workspace paths e.g. thiserror-impl
-                // maybe really use cargo package as there are so many small details
-
-                // cargo package --no-verify --package package-name
-
-                // target/package/*.crate
-
-                // ~/.cargo/registry/cache/index.crates.io-6f17d22bba15001f/
-
-                // diffoscope
-
-                // diffoscope /home/moritz/Documents/cargo-helper/tmp/thiserror-impl\ v1.0.56/target/package/thiserror-impl-1.0.56.crate ~/.cargo/registry/cache/index.crates.io-6f17d22bba15001f/thiserror-impl-1.0.56.crate
-
-                // only package and extract once to reduce disk strain
-
-                let target_directory = {
-                    let command = format!(
-                        r#"(cd "{path_display}/{path_in_vcs}" && cargo metadata --format-version 1 --no-deps | jq --raw-output .target_directory)"#,
-                    );
-                    let output = Command::new("sh")
-                        .arg("-c")
-                        .arg(&command)
-                        .output()
-                        .expect("failed to execute process");
-                    if !output.status.success() {
-                        error!(
-                            "{}\n{}\n{}",
-                            command,
-                            std::str::from_utf8(&output.stderr).unwrap(),
-                            std::str::from_utf8(&output.stdout).unwrap()
-                        );
-                        continue;
-                    } else {
-                        let output = std::str::from_utf8(&output.stdout).unwrap().trim();
-                        if output.is_empty() {
-                            error!("failed to find target directory");
-                            continue;
-                        }
-                        Path::new(output).parent().unwrap().join("target")
-                    }
-                };
-                let target_directory_display = target_directory.display();
-                //println!("the target directory: \"{}\"", target_directory_display);
-
-                if !target_directory.join(".done").exists() {
-                    let command = format!(
-                        r#"(cd "{path_display}/{path_in_vcs}" && cargo package --allow-dirty --no-verify --package {} && tar -xf "{target_directory_display}/package/{}" -C "{target_directory_display}" && touch "{target_directory_display}/{}/.cargo-ok" && touch "{target_directory_display}/.done")"#,
-                        package.name(),
-                        package.package_id().tarball_name(),
-                        package
-                            .package_id()
-                            .tarball_name()
-                            .trim_end_matches(".crate")
-                    );
-                    //println!("{command}");
-                    let output = Command::new("sh")
-                        .arg("-c")
-                        .arg(&command)
-                        .output()
-                        .expect("failed to execute process");
-                    if !output.status.success() {
-                        error!(
-                            "{}\n{}\n{}",
-                            command,
-                            std::str::from_utf8(&output.stderr).unwrap(),
-                            std::str::from_utf8(&output.stdout).unwrap()
-                        );
-                        continue;
-                    }
-                } else {
-                    //println!("already packaged, skipping")
-                }
-
-                /*let registry_source_id = SourceId::alt_registry(
-                    &config,
-                    package.package_id().source_id().alt_registry_key().unwrap(),
-                )
-                .unwrap();*/
-                /*let registry_source_id = SourceId::alt_registry(
-                                    &config,
-                                    package.package_id().source_id().alt_registry_key().unwrap(),
-                                )
-                                .unwrap();
-                                let mut registry_source =
-                                    registry_source_id.load(&config, &HashSet::new()).unwrap();
-                                println!("registry source id: {}", registry_source_id);
-                                let maybe_package = registry_source.download(package.package_id()).unwrap();
-                                println!(
-                                    "tetslfe {:#?}",
-                                    match maybe_package {
-                                        cargo::sources::source::MaybePackage::Ready(file) => file,
-                                        cargo::sources::source::MaybePackage::Download {
-                                            url,
-                                            descriptor,
-                                            authorization,
-                                        } => unreachable!(),
-                                    }
-                                );
-                */
-
-                // mkdir -p tmp/adler\ v1.0.2/target/unpacked  && tar -xf tmp/adler\ v1.0.2/target/package/adler-1.0.2.crate -C tmp/adler\ v1.0.2/target/unpacked
-
-                let crates_io_crate_file = config
-                    .registry_cache_path()
-                    .join(registry_name_with_hash)
-                    .join(package.package_id().tarball_name());
-                //println!("test {}", crates_io_crate_file.as_path_unlocked().display());
-                let _crates_io_crate_file = crates_io_crate_file.display();
-                let crates_io = package.root();
-                let crates_io = crates_io.display();
-
-                // diffoscope --exclude-directory-metadata=yes tmp/adler\ v1.0.2/target/unpacked/adler-1.0.2/ ~/.cargo/registry/src/index.crates.io-6f17d22bba15001f/adler-1.0.2/
-
-                // a few packages are distributed with whitespace changes only
-                let command = format!(
-                    r#"diff -w --color -r --exclude ".github" --exclude ".cargo-ok" --exclude ".cargo_vcs_info.json" --exclude "Cargo.toml" --exclude "Cargo.lock" "{target_directory_display}/{}" "{crates_io}""#,
-                    package
-                        .package_id()
-                        .tarball_name()
-                        .trim_end_matches(".crate"),
-                );
-                // println!("{command}");
-                let output = Command::new("sh")
-                    .arg("-c")
-                    .arg(&command)
-                    .output()
-                    .expect("failed to execute process");
-                if !output.status.success() {
-                    error!(
-                        "{}\n{}\n{}",
-                        command,
-                        std::str::from_utf8(&output.stderr).unwrap(),
-                        std::str::from_utf8(&output.stdout).unwrap()
-                    );
-                    continue;
-                }
             } else {
                 warn!("no vcs version info, bisecting...");
 
@@ -261,13 +124,15 @@ fn main() {
                 let path_display = path.display();
 
                 if !path.join(".git/.done").exists() {
+                    let version = package.version().to_string();
+                    let package_name = package.name().to_string();
                     let command = format!(
-                        r#"(mkdir -p "{path_display}" && cd "{path_display}" && git init && git fetch --filter=tree:0 {url} && git checkout FETCH_HEAD)"#,
+                        r#"(mkdir -p "{path_display}" && cd "{path_display}" && git init && git fetch --filter=tree:0 {url} && git bisect start FETCH_HEAD $(git rev-list --max-parents=0 FETCH_HEAD) && git bisect run sh -c '! echo -e -n "{version}\n$(cargo metadata --format-version=1 --no-deps | jq --raw-output ".packages[] | select(.name == \"{package_name}\") | .version")" | sort -V -C')"#,
                     );
 
                     // maybe find commit by release date? shouldn't make too much sense because maybe you test code and release then
-                    // git bisect start FETCH_HEAD $(git rev-list --max-parents=0 FETCH_HEAD)
-                    // git bisect run sh -c '! echo -e -n "0.3.50\n$(cargo metadata --format-version=1 --no-deps | jq --raw-output ".packages[] | select(.name == \"js-sys\") | .version")" | sort -V -C'
+                    //
+                    // git bisect start FETCH_HEAD $(git rev-list --max-parents=0 FETCH_HEAD) && git bisect run sh -c '! echo -e -n "0.3.50\n$(cargo metadata --format-version=1 --no-deps | jq --raw-output ".packages[] | select(.name == \"js-sys\") | .version")" | sort -V -C'
 
                     //println!("{}", command);
                     let output = Command::new("sh")
@@ -287,6 +152,142 @@ fn main() {
                 } else {
                     //println!("already cloned, skipping")
                 }
+            }
+            // TODO FIXME workspace paths e.g. thiserror-impl
+            // maybe really use cargo package as there are so many small details
+
+            // cargo package --no-verify --package package-name
+
+            // target/package/*.crate
+
+            // ~/.cargo/registry/cache/index.crates.io-6f17d22bba15001f/
+
+            // diffoscope
+
+            // diffoscope /home/moritz/Documents/cargo-helper/tmp/thiserror-impl\ v1.0.56/target/package/thiserror-impl-1.0.56.crate ~/.cargo/registry/cache/index.crates.io-6f17d22bba15001f/thiserror-impl-1.0.56.crate
+
+            // only package and extract once to reduce disk strain
+
+            let target_directory = {
+                let command = format!(
+                    r#"(cd "{path_display}/{path_in_vcs}" && cargo metadata --format-version 1 --no-deps | jq --raw-output .target_directory)"#,
+                );
+                let output = Command::new("sh")
+                    .arg("-c")
+                    .arg(&command)
+                    .output()
+                    .expect("failed to execute process");
+                if !output.status.success() {
+                    error!(
+                        "{}\n{}\n{}",
+                        command,
+                        std::str::from_utf8(&output.stderr).unwrap(),
+                        std::str::from_utf8(&output.stdout).unwrap()
+                    );
+                    continue;
+                } else {
+                    let output = std::str::from_utf8(&output.stdout).unwrap().trim();
+                    if output.is_empty() {
+                        error!("failed to find target directory");
+                        continue;
+                    }
+                    Path::new(output).parent().unwrap().join("target")
+                }
+            };
+            let target_directory_display = target_directory.display();
+            //println!("the target directory: \"{}\"", target_directory_display);
+
+            if !target_directory.join(".done").exists() {
+                let command = format!(
+                    r#"(cd "{path_display}/{path_in_vcs}" && cargo package --allow-dirty --no-verify --package {} && tar -xf "{target_directory_display}/package/{}" -C "{target_directory_display}" && touch "{target_directory_display}/{}/.cargo-ok" && touch "{target_directory_display}/.done")"#,
+                    package.name(),
+                    package.package_id().tarball_name(),
+                    package
+                        .package_id()
+                        .tarball_name()
+                        .trim_end_matches(".crate")
+                );
+                //println!("{command}");
+                let output = Command::new("sh")
+                    .arg("-c")
+                    .arg(&command)
+                    .output()
+                    .expect("failed to execute process");
+                if !output.status.success() {
+                    error!(
+                        "{}\n{}\n{}",
+                        command,
+                        std::str::from_utf8(&output.stderr).unwrap(),
+                        std::str::from_utf8(&output.stdout).unwrap()
+                    );
+                    continue;
+                }
+            } else {
+                //println!("already packaged, skipping")
+            }
+
+            /*let registry_source_id = SourceId::alt_registry(
+                &config,
+                package.package_id().source_id().alt_registry_key().unwrap(),
+            )
+            .unwrap();*/
+            /*let registry_source_id = SourceId::alt_registry(
+                                &config,
+                                package.package_id().source_id().alt_registry_key().unwrap(),
+                            )
+                            .unwrap();
+                            let mut registry_source =
+                                registry_source_id.load(&config, &HashSet::new()).unwrap();
+                            println!("registry source id: {}", registry_source_id);
+                            let maybe_package = registry_source.download(package.package_id()).unwrap();
+                            println!(
+                                "tetslfe {:#?}",
+                                match maybe_package {
+                                    cargo::sources::source::MaybePackage::Ready(file) => file,
+                                    cargo::sources::source::MaybePackage::Download {
+                                        url,
+                                        descriptor,
+                                        authorization,
+                                    } => unreachable!(),
+                                }
+                            );
+            */
+
+            // mkdir -p tmp/adler\ v1.0.2/target/unpacked  && tar -xf tmp/adler\ v1.0.2/target/package/adler-1.0.2.crate -C tmp/adler\ v1.0.2/target/unpacked
+
+            let crates_io_crate_file = config
+                .registry_cache_path()
+                .join(registry_name_with_hash)
+                .join(package.package_id().tarball_name());
+            //println!("test {}", crates_io_crate_file.as_path_unlocked().display());
+            let _crates_io_crate_file = crates_io_crate_file.display();
+            let crates_io = package.root();
+            let crates_io = crates_io.display();
+
+            // diffoscope --exclude-directory-metadata=yes tmp/adler\ v1.0.2/target/unpacked/adler-1.0.2/ ~/.cargo/registry/src/index.crates.io-6f17d22bba15001f/adler-1.0.2/
+
+            // a few packages are distributed with whitespace changes only
+            let command = format!(
+                r#"diff -w --color -r --exclude ".github" --exclude ".cargo-ok" --exclude ".cargo_vcs_info.json" --exclude "Cargo.toml" --exclude "Cargo.lock" "{target_directory_display}/{}" "{crates_io}""#,
+                package
+                    .package_id()
+                    .tarball_name()
+                    .trim_end_matches(".crate"),
+            );
+            // println!("{command}");
+            let output = Command::new("sh")
+                .arg("-c")
+                .arg(&command)
+                .output()
+                .expect("failed to execute process");
+            if !output.status.success() {
+                error!(
+                    "{}\n{}\n{}",
+                    command,
+                    std::str::from_utf8(&output.stderr).unwrap(),
+                    std::str::from_utf8(&output.stdout).unwrap()
+                );
+                continue;
             }
         } else {
             error!("no repository url, can't check anything");
